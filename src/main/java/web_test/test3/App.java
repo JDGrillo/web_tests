9601cad7d1a4db8f4a31010142a1c4567bef7ae3
JDGrillo <jdgrillo@utexas.edu>
2019-07-26 00:38:29 -0500

    adding the rest

---
+++ src/main/java/web_test/test3/App.java
@@ -0,0 +1,25 @@
+package web_test.test3;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    WebDriver driver;
+    
+    @Test
+    public void test() {
+    	System.out.println("test is executing");
+    	System.setProperty("webdriver.chrome.driver", ".\\chromedriver.exe");
+    	ChromeOptions options = new ChromeOptions();
+    	options.addArguments("headless");
+    	driver = new ChromeDriver();
+    	driver.get("http://18.191.120.24");
+    }
+}
